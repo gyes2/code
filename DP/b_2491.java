41764b63e4179ccd385e974535a63830db6d8674
DESKTOP-FA2LLMO\jieun <jieunlee0502@naver.com>
2024-03-07 23:34:05 +0900

    b_2491 진행중

---
+++ DP/b_2491.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class b_2491 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    int[] arr = new int[N];
+    sc.nextLine();
+    for(int i=0; i<N;i++){
+      arr[i] = sc.nextInt();
+    }
+    int cnt = 0;
+    for(int j=0;j<N-1;j++){
+      if
+    }
+    int[] max = new int[N];
+  }
+}
