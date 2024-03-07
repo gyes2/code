8aa96db64442e951fee5455de5e06b2024fc9215
DESKTOP-FA2LLMO\jieun <jieunlee0502@naver.com>
2024-03-07 23:39:33 +0900

    b_2491 진행중

--- DP/b_2491.java
+++ DP/b_2491.java
@@ -10,10 +10,24 @@ public class b_2491 {
     for(int i=0; i<N;i++){
       arr[i] = sc.nextInt();
     }
-    int cnt = 0;
+    int cnt = 1;
+    int pls = 1;
     for(int j=0;j<N-1;j++){
-      if
+      if(arr[j] < arr[j+1] && pls == 1){
+        cnt++;
+      }
+      else if(arr[j] < arr[j+1] && pls == -1){
+        cnt = 1;
+        pls = -1;
+      }
+      else if(arr[j] > arr[j+1] && pls == -1){
+        cnt++;
+      }
+      else{
+        cnt = 1;
+        pls = 1;
+      }
     }
-    int[] max = new int[N];
+    System.out.println(cnt);
   }
 }
